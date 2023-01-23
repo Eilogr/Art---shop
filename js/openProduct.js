5f2ced78953129bb49c5c52cd6273f3694ee38fd
Eilogr <pryadko34@gmail.com>
2023-01-23 15:13:57 +0200

    #

--- js/openProduct.js
+++ js/openProduct.js
@@ -1,5 +1,5 @@
 document.querySelectorAll(".productName").forEach(el => el.addEventListener("click", function() {
-    document.getElementsByClassName('openPosition')[0].style= "display: flex;"
+    document.getElementsByClassName('openPosition')[0].style= "display: block;"
     document.getElementsByClassName('openBG')[0].style= "display: block;"
 }));
 document.querySelectorAll(".openBG").forEach(el => el.addEventListener("click", function() {
