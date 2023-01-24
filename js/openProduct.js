8ac1b892ef28c7f551917b019ffe8d2b5c9a4ad0
Eilogr <pryadko34@gmail.com>
2023-01-24 15:40:20 +0200

    #

--- js/openProduct.js
+++ js/openProduct.js
@@ -1,3 +1,4 @@
+/*OPEN PRODUCT*/ 
 document.querySelectorAll(".productName").forEach(el => el.addEventListener("click", function() {
     document.getElementsByClassName('popup')[0].style= "opacity: 1; visibility: visible;"
 }));
@@ -8,12 +9,35 @@ document.querySelectorAll(".productClose").forEach(el => el.addEventListener("cl
     document.getElementsByClassName('popup')[0].style= "opacity: 0; visibility: invisible;"
 }));
 
+/*OPEN TERMS*/
 document.querySelectorAll(".terms").forEach(el => el.addEventListener("click", function() {
-    document.getElementsByClassName('popup')[0].style= "opacity: 1; visibility: visible;"
+    document.getElementById('popTerms').style= "opacity: 1; visibility: visible;"
 }));
 document.querySelectorAll(".closeArea").forEach(el => el.addEventListener("click", function() {
-    document.getElementsByClassName('popup')[0].style= "opacity: 0; visibility: invisible;"
+    document.getElementById('popTerms').style= "opacity: 0; visibility: invisible;"
 }));
 document.querySelectorAll(".productClose").forEach(el => el.addEventListener("click", function() {
-    document.getElementsByClassName('popup')[0].style= "opacity: 0; visibility: invisible;"
+    document.getElementById('popTerms').style= "opacity: 0; visibility: invisible;"
+}));
+
+/*OPEN TERMS*/
+document.querySelectorAll(".artists").forEach(el => el.addEventListener("click", function() {
+    document.getElementById('popArtists').style= "opacity: 1; visibility: visible;"
+}));
+document.querySelectorAll(".closeArea").forEach(el => el.addEventListener("click", function() {
+    document.getElementById('popArtists').style= "opacity: 0; visibility: invisible;"
+}));
+document.querySelectorAll(".productClose").forEach(el => el.addEventListener("click", function() {
+    document.getElementById('popArtists').style= "opacity: 0; visibility: invisible;"
+}));
+
+/*OPEN BUSKET*/
+document.querySelectorAll(".busket").forEach(el => el.addEventListener("click", function() {
+    document.getElementById('popBusket').style= "opacity: 1; visibility: visible;"
+}));
+document.querySelectorAll(".closeArea").forEach(el => el.addEventListener("click", function() {
+    document.getElementById('popBusket').style= "opacity: 0; visibility: invisible;"
+}));
+document.querySelectorAll(".productClose").forEach(el => el.addEventListener("click", function() {
+    document.getElementById('popBusket').style= "opacity: 0; visibility: invisible;"
 }));
